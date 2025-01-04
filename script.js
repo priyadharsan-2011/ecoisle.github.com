4d3dc5aa6f62abeeb6aca8b438a9af7e4c6f2516
priyadharsan-2011 <priyadharsan.m136@gmail.com>
2025-01-04 21:44:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Form validation function
+function validateForm() {
+    const message = document.getElementById("message").value.trim();
+    if (!message) {
+        alert("Message field cannot be empty!");
+        return false;
+    }
+    return true;
+}
